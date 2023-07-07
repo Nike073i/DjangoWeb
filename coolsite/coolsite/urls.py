55c78a9c8318931295f7ed2ba101912fca99a798
Nike073i <nike073i@mail.ru>
2023-07-07 18:44:41 +0400

    Creating a model for articles (about women) and migration file

--- coolsite/coolsite/urls.py
+++ coolsite/coolsite/urls.py
@@ -1,11 +1,15 @@
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
+from coolsite import settings
 from women.views import page_not_found
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('women.urls')),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_URL)
 
 handler404 = page_not_found
